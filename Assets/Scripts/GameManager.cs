ba9171c9027483e74864a7a5e8511e1b72b9fcd9
Pyorang <sudangi143@gmail.com>
2023-10-07 20:40:50 +0900

    lesson26

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public TalkManager talkManager;
+    public Text talkText;
+    public Image portraitImg;
+    public GameObject scanObject;
+    public GameObject talkPanel;
+    public bool isAction;
+    public int talkIndex;
+
+    public void Action(GameObject scanObj)
+    {
+        scanObject = scanObj;
+        ObjtData objData = scanObject.GetComponent<ObjtData>();
+        Talk(objData.id, objData.isNpc);
+        talkPanel.SetActive(isAction);
+    }
+
+    void Talk(int id, bool isNpc)
+    {
+        Debug.Log("id" + id);
+        Debug.Log("isNpc" + isNpc);
+
+
+        string talkData = talkManager.GetTalk(id, talkIndex);
+
+        if (talkData == null)
+        {
+            isAction = false;
+            talkIndex = 0;
+            return;
+        }
+
+        if (isNpc)
+        {
+            talkText.text = talkData.Split(':')[0];
+
+            portraitImg.sprite = talkManager.GetPortrait(id, int.Parse(talkData.Split(':')[1]));
+            portraitImg.color = new Color(1, 1, 1, 1);
+        }
+
+        else
+        {
+            talkText.text = talkData;
+
+            portraitImg.color = new Color(1, 1, 1, 0);
+        }
+
+        isAction = true;
+        talkIndex++;
+    }
+}
